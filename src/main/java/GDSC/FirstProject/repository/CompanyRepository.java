4e6e3e23729fcea186d44885593594ae9ef832cf
sami355-24 <ddung355@gmail.com>
2023-02-25 18:44:59 +0900

    [Feat]: Company, FeedHashtag, Feed, Hashtag repository의 알맞은 메서드 추가, 반환값을 Optional객체를 반환하도록 수정

--- src/main/java/GDSC/FirstProject/repository/CompanyRepository.java
+++ src/main/java/GDSC/FirstProject/repository/CompanyRepository.java
@@ -3,7 +3,12 @@ package GDSC.FirstProject.repository;
 import GDSC.FirstProject.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
     boolean existsByvalue(String company_name);
+
+    Optional<Company> findByvalue(String company_name);
+
 }
--- src/main/java/GDSC/FirstProject/repository/HashtagRepository.java
+++ src/main/java/GDSC/FirstProject/repository/HashtagRepository.java
@@ -3,8 +3,11 @@ package GDSC.FirstProject.repository;
 import GDSC.FirstProject.entity.Hashtag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface HashtagRepository extends JpaRepository<Hashtag, Long>{
 
     boolean existsAllByvalueIn(String[] value);
 
+    Optional<Hashtag> findByvalue(String value);
 }
