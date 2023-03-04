71f3e4bf8d1e5316ff6b3c528508c29b624ab333
sami355-24 <ddung355@gmail.com>
2023-03-04 19:56:26 +0900

    [Refactor]: 기존의 controller에 존재하는 코드 삭제후 서비스 패키지에 존재하는 FeedService 클래스에 의존하도록 수정

--- src/main/java/GDSC/FirstProject/controller/FeedController.java
+++ src/main/java/GDSC/FirstProject/controller/FeedController.java
@@ -1,10 +1,7 @@
 package GDSC.FirstProject.controller;
 
 import GDSC.FirstProject.dto.requsetDto.createFeedRequestDto;
-import GDSC.FirstProject.entity.*;
-import GDSC.FirstProject.repository.*;
-import GDSC.FirstProject.s3.S3Uploader;
-import GDSC.FirstProject.service.impl.FeedServiceImpl;
+import GDSC.FirstProject.service.FeedService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,29 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class FeedController {
 
-    public final MemberRepository memberRepository;
-    public final CompanyRepository companyRepository;
-    public final HashtagRepository hashtagRepository;
-    public final FeedHashtagRepository feedHashtagRepository;
-    public final FeedRepository feedRepository;
-    public final FeedServiceImpl feedService;
-    public final S3Uploader s3Uploader;
+    private final FeedService feedService;
+
     @PostMapping
     public void saveFeed(@RequestBody createFeedRequestDto requestDto){
-
-        Member findMember = memberRepository.findById(Long.valueOf(requestDto.uid)).orElseThrow(() -> new IllegalArgumentException("유저가 존재하지 않습니다."));
-
-        String RandomS3Key = feedService.makeRandomS3Key();
-
-//        s3Uploader.upload(requestDto.image, RandomS3Key);
-
-        Company findCompany = companyRepository.findByvalue(requestDto.company).orElseThrow(()-> new IllegalArgumentException("회사가 존재하지 않습니다."));
-
-        Feed saveFeed = feedRepository.save(new Feed(requestDto, RandomS3Key, findMember, findCompany));
-
-        for (String hashtag : requestDto.hashtags) {
-            Hashtag findHashtag = hashtagRepository.findByvalue(hashtag).orElseThrow(() -> new IllegalArgumentException("해시태그가 존재하지 않습니다."));
-            feedHashtagRepository.save(new FeedHashtag(saveFeed, findHashtag));
-        }
+        feedService.saveFeed(requestDto);
     }
 }
