1c1b5c8d90da80a14fa988b2cac582e0b72ef605
lee-ji-an <leeja0407@naver.com>
2023-03-08 00:28:17 +0900

    [Chore]: 멤버 컨트롤러 매핑 url 번경

--- src/main/java/GDSC/FirstProject/controller/MemberController.java
+++ src/main/java/GDSC/FirstProject/controller/MemberController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/member")
 public class MemberController {
 
     private final MemberService memberService;
