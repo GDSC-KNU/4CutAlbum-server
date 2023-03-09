517358a9f6390c2f9bdf65768e15e9317d6953d8
sami355-24 <ddung355@gmail.com>
2023-03-09 23:51:06 +0900

    [Test]: 피드리스트 조회 repository 올바르게 동작하는지 테스트

--- src/test/java/GDSC/FirstProject/repository/FeedRepositoryTest.java
+++ src/test/java/GDSC/FirstProject/repository/FeedRepositoryTest.java
@@ -1,10 +1,14 @@
 package GDSC.FirstProject.repository;
 
-import GDSC.FirstProject.dto.dbDto.feedListDbDto;
+import GDSC.FirstProject.dto.dbDto.originalFeedListDbDto;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
+import org.springframework.data.domain.Sort;
 
 import java.util.List;
 
@@ -15,20 +19,29 @@ public class FeedRepositoryTest {
     FeedRepository feedRepository;
 
     @Test
+    @DisplayName("피드리스트 조회 테스트")
     public void findFeedListTest() throws Exception {
         //given
         String company_name = "인생네컷";
         Long people_count = 2L;
         String[] hashtags = new String[]{"test1", "test2"};
+        PageRequest pageRequest = PageRequest.of(1, 5, Sort.by(Sort.Direction.DESC, "createdDate"));
 
         //when
-        List<feedListDbDto> feedList = feedRepository.findFeedList(company_name, people_count, hashtags)
-                .orElseThrow(() -> new Exception("feedList is null"));
+        Slice<originalFeedListDbDto> slice = feedRepository.findFeedList(company_name, people_count, hashtags, pageRequest);
+        List<originalFeedListDbDto> dtos = slice.getContent();
+        boolean hasNext = slice.hasNext();
 
         //then
-        Assertions.assertThat(feedList).isNotNull();
-        for (GDSC.FirstProject.dto.dbDto.feedListDbDto feedListDbDto : feedList) {
-            System.out.println("feedListDbDto = " + feedListDbDto.toString());
+        Assertions.assertThat(dtos.size()).isEqualTo(5);
+        Assertions.assertThat(hasNext).isTrue();
+
+        System.out.println(dtos.getClass());
+        System.out.println(slice.getNumber());
+        for (originalFeedListDbDto dto : dtos) {
+            System.out.println(dto.toString());
         }
+
+
     }
 }
