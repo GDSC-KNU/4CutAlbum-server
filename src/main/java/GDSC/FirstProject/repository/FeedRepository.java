e13e1ae8983cda25924a2ecdd7ac54566b12ad14
sami355-24 <ddung355@gmail.com>
2023-03-09 13:19:31 +0900

    [Feat]: findFeedList 메서드의 쿼리문에 인자를 넣을수 있게끔 코드 수정

--- src/main/java/GDSC/FirstProject/repository/FeedRepository.java
+++ src/main/java/GDSC/FirstProject/repository/FeedRepository.java
@@ -1,9 +1,13 @@
 package GDSC.FirstProject.repository;
 
+import GDSC.FirstProject.dto.dbDto.feedListDbDto;
 import GDSC.FirstProject.entity.Company;
 import GDSC.FirstProject.entity.Feed;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
@@ -11,4 +15,15 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
     Optional<Feed> findBycompany(Company company);
 
     Optional<Feed> findById(Long id);
+
+    @Query(
+            " select new GDSC.FirstProject.dto.dbDto.feedListDbDto(f.id, f.s3Key, f.peopleCount, c.value, h.value)" +
+                    " from Feed f" +
+                    "          inner join Company c on f.company = c" +
+                    "          inner join FeedHashtag fh on f = fh.feed" +
+                    "          inner join Hashtag h on fh.hashtag = h" +
+                    " where c.value = :company_name and f.peopleCount = :people_count and h.value IN :hashtags" +
+                    " order by f.createdDate desc"
+    )
+    Optional<List<feedListDbDto>> findFeedList(@Param("company_name") String company_name, @Param("people_count") Long people_count, @Param("hashtags") String[] hashtags);
 }
