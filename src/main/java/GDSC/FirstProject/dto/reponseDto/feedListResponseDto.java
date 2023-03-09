7dc38dbb06da6423a09ad52c16b862b10e6a98dc
sami355-24 <ddung355@gmail.com>
2023-03-09 23:48:36 +0900

    [Refactor]: API 스펙에 맞게끔 Dto 수정

--- src/main/java/GDSC/FirstProject/dto/reponseDto/feedListResponseDto.java
+++ src/main/java/GDSC/FirstProject/dto/reponseDto/feedListResponseDto.java
@@ -1,24 +1,17 @@
 package GDSC.FirstProject.dto.reponseDto;
 
-
+import GDSC.FirstProject.dto.dbDto.distinctFeedListDbDto;
+import lombok.Builder;
 import lombok.Data;
 import lombok.ToString;
 
 @Data
+@Builder
 @ToString
 public class feedListResponseDto {
 
-    public Long feed_id;
-    public String image;
-    public Long people_count;
-    public String company_name;
-    public String[] hashtag;
+    distinctFeedListDbDto[] feedList;
+    Long page_number;
+    boolean hasNext;
 
-    public feedListResponseDto(Long feed_id, String image, Long people_count, String company_name, String[] hashtag) {
-        this.feed_id = feed_id;
-        this.image = image;
-        this.people_count = people_count;
-        this.company_name = company_name;
-        this.hashtag = hashtag;
-    }
 }
--- src/main/java/GDSC/FirstProject/dto/requsetDto/feedListRequestDto.java
+++ src/main/java/GDSC/FirstProject/dto/requsetDto/feedListRequestDto.java
@@ -4,12 +4,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.List;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class feedListRequestDto {
 
     public String company;
-    public Integer people_count;
-    public String[] hashtag;
+    public Long people_count;
+    public List<String> hashtag;
 }
