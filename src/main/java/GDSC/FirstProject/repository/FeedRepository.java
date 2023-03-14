2e51221cfb6e7081b265384997975c5ded97ee87
sami355-24 <ddung355@gmail.com>
2023-03-14 16:20:54 +0900

    [Feat]: feedRepository에 DbDto로 조회하는 메서드 추가

--- src/main/java/GDSC/FirstProject/repository/FeedRepository.java
+++ src/main/java/GDSC/FirstProject/repository/FeedRepository.java
@@ -1,5 +1,6 @@
 package GDSC.FirstProject.repository;
 
+import GDSC.FirstProject.dto.dbDto.feedInfoDbDto;
 import GDSC.FirstProject.dto.dbDto.originalFeedListDbDto;
 import GDSC.FirstProject.entity.Company;
 import GDSC.FirstProject.entity.Feed;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface FeedRepository extends JpaRepository<Feed, Long> {
@@ -32,4 +34,14 @@ public interface FeedRepository extends JpaRepository<Feed, Long> {
             @Param("hashtags") String[] hashtags,
             Pageable pageable
     );
+
+    @Query(" select new GDSC.FirstProject.dto.dbDto.feedInfoDbDto( f.id, m.nickName, f.s3Key, c.value, f.likes, f.comment, h.value, f.peopleCount) " +
+            " from Feed f" +
+            "          inner join Company c on f.company = c" +
+            "          inner join FeedHashtag fh on f = fh.feed" +
+            "          inner join Hashtag h on fh.hashtag = h" +
+            "          inner join Member m on f.member = m" +
+            " where f.id = :id"
+    )
+    Optional<List<feedInfoDbDto>> findFeedInfo(@Param("id") Long id);
 }
