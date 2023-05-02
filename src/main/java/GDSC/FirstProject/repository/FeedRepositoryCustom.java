942924fe651b8962096256e2d0047c6f959a081d
sami355-24 <ddung355@gmail.com>
2023-05-02 10:57:52 +0900

    [Feat]: FeedRepository Interface수정, FeedRepository Implementation 수정

--- src/main/java/GDSC/FirstProject/repository/FeedRepositoryCustom.java
+++ src/main/java/GDSC/FirstProject/repository/FeedRepositoryCustom.java
@@ -1,13 +1,21 @@
 package GDSC.FirstProject.repository;
 
-import GDSC.FirstProject.dto.dbDto.originalFeedListDbDto;
+import GDSC.FirstProject.dto.dbDto.PartOfFeedListDbDto;
+import GDSC.FirstProject.dto.dbDto.feedListDbDto;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.data.repository.query.Param;
 
 public interface FeedRepositoryCustom {
 
-    Slice<originalFeedListDbDto> findFeedList_Querydsl(
+    Slice<feedListDbDto> findFeedList_QuerydslFixed(
+            @Param("company_name") String company_name,
+            @Param("people_count") Long people_count,
+            @Param("hashtags") String[] hashtags,
+            Pageable pageable
+    );
+
+    Slice<PartOfFeedListDbDto> findPartOfFeedList_Querydsl(
             @Param("company_name") String company_name,
             @Param("people_count") Long people_count,
             @Param("hashtags") String[] hashtags,
