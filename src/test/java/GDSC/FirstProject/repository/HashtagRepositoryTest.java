6c5d52e0fc3dfc4fac79f70d8d069cf7831c2f6a
sami355-24 <ddung355@gmail.com>
2023-05-11 00:35:06 +0900

    [Test]: 해시태그들 전부 가져오는 메서드 test

--- src/test/java/GDSC/FirstProject/repository/HashtagRepositoryTest.java
+++ src/test/java/GDSC/FirstProject/repository/HashtagRepositoryTest.java
@@ -1,9 +1,13 @@
 package GDSC.FirstProject.repository;
 
+import GDSC.FirstProject.entity.Hashtag;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootTest
 class HashtagRepositoryTest {
@@ -12,7 +16,7 @@ class HashtagRepositoryTest {
     HashtagRepository hashtagRepository;
 
     @Test
-    public void existsByvalueTest() throws Exception{
+    public void existsByvalueTest() throws Exception {
         //given
         String values[] = {"test", "test2"};
 
@@ -21,5 +25,37 @@ class HashtagRepositoryTest {
 
         //then
         Assertions.assertThat(TF).isTrue();
+    }
+
+    @Test
+    public void saveHashtag() throws Exception {
+        //given
+        List<Hashtag> hashtags = new ArrayList<>();
+        hashtags.add(new Hashtag("졸업"));
+        hashtags.add(new Hashtag("생일"));
+        hashtags.add(new Hashtag("커플"));
+        hashtags.add(new Hashtag("우정샷"));
+
+        //when
+        hashtagRepository.saveAll(hashtags);
+
+        //then
+        List<Hashtag> findHashtags = hashtagRepository.findAll();
+        for (Hashtag findHashtag : findHashtags) {
+            System.out.println(findHashtag.getValue());
+        }
+    }
+
+    @Test
+    public void findAllHashtag() throws Exception{
+        //given
+
+        //when
+        List<Hashtag> result = hashtagRepository.findAll();
+
+        //then
+        for(Hashtag hashtag : result){
+            System.out.println(hashtag.getValue());
+        }
     }
 }
