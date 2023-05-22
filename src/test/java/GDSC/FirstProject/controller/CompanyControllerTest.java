9e7253ec9a93018b3e2173609f62eabbdfd3b5a2
sami355-24 <ddung355@gmail.com>
2023-05-22 20:07:40 +0900

    [Test]: CompanyController에 추가한 findCompanyList를 테스트하는 메서드 추가

---
+++ src/test/java/GDSC/FirstProject/controller/CompanyControllerTest.java
@@ -0,0 +1,51 @@
+package GDSC.FirstProject.controller;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.filter.CharacterEncodingFilter;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@AutoConfigureMockMvc
+@SpringBootTest
+class CompanyControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private WebApplicationContext ctx;
+
+    @BeforeEach
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(ctx)
+                .addFilters(new CharacterEncodingFilter("UTF-8", true))
+                .alwaysDo(print())
+                .build();
+    }
+
+    @Test
+    public void findCompanyListTest() throws Exception{
+        //given
+
+
+        //when
+        MvcResult mvcResult = mockMvc.perform(MockMvcRequestBuilders.get("/company"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        //then
+        String result = mvcResult.getResponse().getContentAsString();
+        System.out.println(result);
+    }
+
+}
